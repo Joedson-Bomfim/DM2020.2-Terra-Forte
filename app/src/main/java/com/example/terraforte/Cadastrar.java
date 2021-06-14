0af387a7e9feff12af9ae0d6a5be9c231136b907
Joedson <joedsoncte@gmail.com>
2021-06-14 16:13:04 -0300

    Arquivos java criados para cada layout

---
+++ app/src/main/java/com/example/terraforte/Cadastrar.java
@@ -0,0 +1,14 @@
+package com.example.terraforte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastrar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrar);
+    }
+}
