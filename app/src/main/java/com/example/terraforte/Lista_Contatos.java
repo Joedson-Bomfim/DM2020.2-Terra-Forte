db5b2436944df2497efedb802a451697b19c206e
Joedson <joedsoncte@gmail.com>
2021-06-15 16:41:39 -0300

    Telas de contatos e receituários

---
+++ app/src/main/java/com/example/terraforte/Lista_Contatos.java
@@ -0,0 +1,14 @@
+package com.example.terraforte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista_Contatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_contatos);
+    }
+}
---
+++ app/src/main/java/com/example/terraforte/Receituarios.java
@@ -0,0 +1,14 @@
+package com.example.terraforte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Receituarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receituarios);
+    }
+}
