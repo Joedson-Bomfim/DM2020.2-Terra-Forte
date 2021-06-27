b05dff6526a0ad237051d7eaac16db0aca909760
Joedson <joedsoncte@gmail.com>
2021-06-27 15:22:30 -0300

    Classe usuario e banco SQLite criado

---
+++ app/src/main/java/com/example/terraforte/Usuario.java
@@ -0,0 +1,85 @@
+package com.example.terraforte;
+
+public class Usuario {
+    private int idUsuario;
+    private String email;
+    private String senha;
+    private String nomeUsuario;
+    private String nomeCompleto;
+    private String apelido;
+    private String telefone;
+    private String endereco;
+    private boolean funcao;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setFuncao(String telefone) {
+        this.funcao = funcao;
+    }
+
+    public String getFuncao() {
+        return endereco;
+    }
+}
---
+++ app/src/main/java/com/example/terraforte/UsuarioDAO.java
@@ -0,0 +1,58 @@
+package com.example.terraforte;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UsuarioDAO extends SQLiteOpenHelper {
+
+    public UsuarioDAO(Context context) {
+        super(context, "Agenda", null, 1);
+    }
+
+    //public UsuarioDAO(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+    //    super(context, name, factory, version);
+    //}
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE IF NOT EXISTS usuario (" +
+                "idUsuario INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "email TEXT NOT NULL, " +
+                "senha TEXT NOT NULL, " +
+                "nomeusuario TEXT NOT NULL, " +
+                "nomecompleto TEXT NOT NULL, " +
+                "apelido TEXT, " +
+                "telefone TEXT NOT NULL, " +
+                "endereco TEXT NOT NULL, " +
+                "funcao BOOLEAN NOT NULL); ";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void cadastrar(Usuario usuario) {
+        SQLiteDatabase conexao = getWritableDatabase();
+
+        ContentValues dados = new ContentValues();
+
+        dados.put("email", usuario.getEmail());
+        dados.put("senha", usuario.getSenha());
+        dados.put("nomeUsuario", usuario.getNomeUsuario());
+        dados.put("nomeCompleto", usuario.getNomeCompleto());
+        dados.put("apelido", usuario.getApelido());
+        dados.put("telefone", usuario.getTelefone());
+        dados.put("endereco", usuario.getEndereco());
+        dados.put("funcao", usuario.getFuncao());
+
+        conexao.insertOrThrow("contato", null, dados);
+    }
+}
