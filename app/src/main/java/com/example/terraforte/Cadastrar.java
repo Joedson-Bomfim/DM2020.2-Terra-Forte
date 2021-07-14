b1d133d63958a2bfd31742ab8159d42df046c30f
Joedson <joedsoncte@gmail.com>
2021-07-13 21:51:39 -0300

    Cadastro de usuario e dados pronto-falta o CRUD

--- app/src/main/java/com/example/terraforte/Cadastrar.java
+++ app/src/main/java/com/example/terraforte/Cadastrar.java
@@ -17,13 +17,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cadastrar extends AppCompatActivity {
     private EditText email;
@@ -39,6 +48,8 @@ public class Cadastrar extends AppCompatActivity {
     private RadioButton radioButtonEscolhido;
 
     private UsuarioDAO dao;
+
+    String usuarioID;
 
     @SuppressLint("WrongConstant")
     @Override
@@ -64,7 +75,7 @@ public class Cadastrar extends AppCompatActivity {
         dao = new UsuarioDAO(this);
     }
 
-    public void acessarHome(View v) {
+    public void acessarLogin(View v) {
         if(email.getText().toString().isEmpty() || senha.getText().toString().isEmpty()){
             Toast.makeText(Cadastrar.this, "Preencha o email!", Toast.LENGTH_SHORT).show();
         } else {
@@ -80,16 +91,62 @@ public class Cadastrar extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
-                    Snackbar snackbar = Snackbar.make(v, "Cadastrado com sucesso",Snackbar.LENGTH_SHORT);
+                    SalvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(v, "Cadastrado com sucesso",Snackbar.LENGTH_LONG);
                     snackbar.setBackgroundTint(Color.GREEN);
                     snackbar.setTextColor(Color.WHITE);
                     snackbar.show();
                 }else {
-                    Snackbar snackbar = Snackbar.make(v, "Problema no cadastro",Snackbar.LENGTH_SHORT);
+                    String erro;
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        erro = "Digite uma senha com no mínimo 6 caractéres";
+                    }catch (FirebaseAuthUserCollisionException e) {
+                        erro = "Esta conta já foi  cadastrada";
+                    }catch (FirebaseAuthInvalidCredentialsException e) {
+                        erro = "E-mail inválido";
+                    }catch (Exception e){
+                        erro = "Erro ao cadastrar usuário";
+                    }
+                    Snackbar snackbar = Snackbar.make(v, erro,Snackbar.LENGTH_LONG);
                     snackbar.setBackgroundTint(Color.RED);
                     snackbar.setTextColor(Color.WHITE);
                     snackbar.show();
                 }
+            }
+        });
+    }
+
+    private void SalvarDadosUsuario() {
+        String edit_nome_usuario = nome_usuario.getText().toString();
+        String edit_nome_completo = nome_completo.getText().toString();
+        String edit_apelido = apelido.getText().toString();
+        String edit_telefone = telefone.getText().toString();
+        String edit_endereco = endereco.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome_usuario", edit_nome_usuario);
+        usuarios.put("nome_completo",edit_nome_completo);
+        usuarios.put("apelido", edit_apelido);
+        usuarios.put("telefone", edit_telefone);
+        usuarios.put("endereco", edit_endereco);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                Log.d("db", "Sucesso ao salvar os dados");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error", "Erro ao salvar os dados");
             }
         });
     }
