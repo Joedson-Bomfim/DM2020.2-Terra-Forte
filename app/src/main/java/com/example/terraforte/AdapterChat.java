e133e3a5d8ae84927c38c482c39aef545d15e9a2
Joedson <joedsoncte@gmail.com>
2021-08-07 23:51:04 -0300

    Baixar imagens pelo chat em andamento

--- app/src/main/java/com/example/terraforte/AdapterChat.java
+++ app/src/main/java/com/example/terraforte/AdapterChat.java
@@ -1,8 +1,10 @@
 package com.example.terraforte;
 
 import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +16,12 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.view.menu.MenuView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,6 +30,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.text.DateFormat;
@@ -31,6 +40,8 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
 
 public class AdapterChat extends RecyclerView.Adapter<AdapterChat.Myholer> {
 
@@ -73,6 +84,7 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.Myholer> {
         if(type.equals("text")) {
             myholer.messageTv.setVisibility(View.VISIBLE);
             myholer.messageIv.setVisibility(View.GONE);
+            myholer.download_tv.setVisibility(View.GONE);
 
             myholer.messageTv.setText(message);
         }else {
@@ -172,8 +184,12 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.Myholer> {
     class Myholer extends RecyclerView.ViewHolder{
 
         ImageView messageIv;
-        TextView messageTv, timeTv, isSeenTv;
+        TextView messageTv, timeTv, isSeenTv, download_tv;
         LinearLayout messageLayout;
+
+        FirebaseStorage firebaseStorage;
+        StorageReference storageReference;
+        StorageReference ref;
 
         public Myholer(@NonNull View itemView) {
             super(itemView);
@@ -183,6 +199,44 @@ public class AdapterChat extends RecyclerView.Adapter<AdapterChat.Myholer> {
             timeTv = itemView.findViewById(R.id.timeTv);
             isSeenTv = itemView.findViewById(R.id.isSentTv);
             messageLayout = itemView.findViewById(R.id.messageLayout);
+            download_tv = itemView.findViewById(R.id.downloadtv_Chat);
+
+            download_tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    download();
+                }
+            });
+        }
+
+        private void download() {
+            //Obs: Os arquivos pdf exigem o .pdf nome nome enquanto que as imagens dão erro caso coloque o formato no child()
+            storageReference = firebaseStorage.getInstance().getReference();
+            ref = storageReference.child("ChatImage/post_1628387419724");
+
+            ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    String url = uri.toString();
+                    downloadFiles(context, "pdf1", ".png", DIRECTORY_DOWNLOADS, url);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(context, "Erro ao baixar a imagem", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        private void downloadFiles(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+            DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            Uri uri = Uri.parse(url);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+            downloadManager.enqueue(request);
         }
     }
 }
